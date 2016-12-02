55d65a24f3814c3410454d291768576430cd4a59
Joe Bruckner <joembruckner@gmail.com>
2016-12-02 00:46:05 -0500

    Allow teachers to add students to a course

--- UniversitiesApp/forms.py
+++ UniversitiesApp/forms.py
@@ -5,13 +5,19 @@ Created by Jacob Dunbar on 11/5/2016.
 """
 from django import forms
 
+
 class UniversityForm(forms.Form):
     name = forms.CharField(label='Name', max_length=50)
     photo = forms.ImageField(label='Photo')
     description = forms.CharField(label='Description', max_length=300)
-    website = forms.CharField(label='Website', max_length = 300)
-	
+    website = forms.CharField(label='Website', max_length=300)
+
+
 class CourseForm(forms.Form):
-	tag = forms.CharField(label='Tag', max_length=10)
-	name = forms.CharField(label='Name', max_length=50)
-	description = forms.CharField(label='Description', max_length=300)
+    tag = forms.CharField(label='Tag', max_length=10)
+    name = forms.CharField(label='Name', max_length=50)
+    description = forms.CharField(label='Description', max_length=300)
+
+
+class StudentForm(forms.Form):
+    email = forms.CharField(label='Email', max_length=50)
